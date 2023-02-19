2623054000a26fe72f720eede54f31c6441b9e6f
Shihab Ahmed Pranto <sa.pranto16@gmail.com>
2023-02-19 21:07:24 +0600

    Implemented UseState On TabsMenu

--- src/components/TabMenu.jsx
+++ src/components/TabMenu.jsx
@@ -1,23 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { AiFillFire, AiFillCarryOut } from "react-icons/ai";
 import { GiRunningShoe } from "react-icons/gi";
-const TabMenu = () => {
-  return (
-          <ul className="flex flex-wrap items-center py-10 justify-center bg-white text-cyan-800 gap-10 md:gap-20 md:text-lg font-semibold ">
-          <li className="flex gap-1 md:gap-2 cursor-pointer">
-            <GiRunningShoe className="md:mt-1 text-green-700 text-xl " />
-            New Arrivals
-          </li>
-          <li className="flex gap-1 md:gap-2 cursor-pointer">
-            <AiFillFire className="md:mt-1 text-red-600 text-xl" />
-            Most Selling
-          </li>
-          <li className="flex gap-1 md:gap-2 cursor-pointer">
-            <AiFillCarryOut className="md:mt-1 text-sky-400 text-xl" />
-            Pre-Order
-          </li>
-        </ul>
-  )
-}
 
-export default TabMenu
+const TabMenu = () => {
+  const [activeTab, setActiveTab] = useState('new-arrivals');
+
+  const handleTabClick = (tabName) => {
+    setActiveTab(tabName);
+  };
+
+  return (
+    <ul className="flex flex-wrap items-center py-10 justify-center bg-white text-cyan-800 gap-10 md:gap-20 md:text-lg font-semibold">
+      <li className={`flex gap-1 md:gap-2 cursor-pointer ${activeTab === 'new-arrivals' ? 'scale-125 border-b border-blue-600 transition duration-300 ease-in-out' : ''}`} onClick={() => handleTabClick('new-arrivals')}>
+        <GiRunningShoe className="md:mt-1 text-green-700 text-xl" />
+        New Arrivals
+      </li>
+      <li className={`flex gap-1 md:gap-2 cursor-pointer ${activeTab === 'most-selling' ? 'scale-125 border-b border-blue-600 transition duration-300 ease-in-out' : ''}`} onClick={() => handleTabClick('most-selling')}>
+        <AiFillFire className="md:mt-1 text-red-600 text-xl" />
+        Most Selling
+      </li>
+      <li className={`flex gap-1 md:gap-2 cursor-pointer ${activeTab === 'pre-order' ? 'scale-125 border-b border-blue-600 transition duration-300 ease-in-out' : ''}`} onClick={() => handleTabClick('pre-order')}>
+        <AiFillCarryOut className="md:mt-1 text-sky-400 text-xl" />
+        Pre-Order
+      </li>
+    </ul>
+  );
+};
+
+export default TabMenu;
