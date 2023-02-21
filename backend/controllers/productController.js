5f80ab5063f88b675a859bca8b7c6f1d67dbfba8
Shihab Ahmed Pranto <sa.pranto16@gmail.com>
2023-02-21 11:44:29 +0600

    Created Create Products Route and json

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,3 +1,15 @@
+const Product = require("../models/product");
+
+// Create New Product
+
+exports.newProduct = async (req, res) => {
+  const product = await Product.create(req.body);
+  res.status(201).json({
+    success:true,
+    product
+  })
+};
+
 exports.getProducts = (req, res, next) => {
   res.status(200).json({
     success: true,
